d12766575ce6f857c702d4a3e959c986d19837af
Andrew <andrey-doncow@yandex.ru>
2020-03-04 21:34:54 +0300

    Пробный коммит

---
+++ Assets/Scripts/CubeBehavior.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeBehavior : MonoBehaviour
+{
+
+    public float _speed;
+    private float _timer;
+    private Rigidbody rb;
+    private int _countDestroy;
+    
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        _countDestroy = 0;
+    }
+    void OnMouseDown()
+    {
+        Destroy(gameObject);
+    }
+
+    void OnTriggerEnter(BoxCollider other)
+    {
+        rb.AddForce(transform.forward*0.1f);
+    }
+
+    void Update()
+    {
+
+        _timer += Time.deltaTime;
+        
+        transform.localScale *=  (1+Time.deltaTime/_speed);
+        if (_timer > _speed)
+        {
+            Destroy(gameObject);
+            _countDestroy++;
+        }
+
+        if (_countDestroy >= 10 && _speed > 1)
+        {
+            _speed -= 0.5f;
+        }
+
+
+    }
+
+}
---
+++ Assets/Scripts/Spawner.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    private float del;
+    [SerializeField]
+    private GameObject Cube;
+    private float speed; 
+
+    void Start()
+    {
+       speed =  GetComponent<CubeBehavior>()._speed;
+}
+    private void Update()
+    {
+       
+        del += Time.deltaTime;
+        if (del > speed/10)
+        {
+            Instantiate(Cube, new Vector2(Random.Range(-12f, 12f), Random.Range(-4.5f, 4.5f)),
+           Quaternion.identity);
+            del = 0;
+        }
+    }
+}

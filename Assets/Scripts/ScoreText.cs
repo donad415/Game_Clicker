fc4535f615cc090e81b1eec784ea1b80e5b2fef5
Andrew <andrey-doncow@yandex.ru>
2020-03-05 12:20:23 +0300

    Пробный

---
+++ Assets/Scripts/ScoreText.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreText : MonoBehaviour
+{
+    public int score;
+    public Text text;
+
+
+    void Start()
+    {
+        score = 0;
+        text = gameObject.GetComponent<Text>();
+    }
+
+    void Update()
+    {
+
+        text.text = score + "";
+    }
+}
---
+++ Assets/Scripts/SphereBehaviour.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SphereBehaviour : MonoBehaviour
+{
+    private float _timer;
+    private Spawner sp;
+    private ScoreText score;
+
+    void Start()
+    {
+        score = GameObject.Find("Score").GetComponent<ScoreText>();
+        sp = GameObject.Find("Spawner").GetComponent<Spawner>();
+    }
+
+    void OnMouseDown()
+    {
+        Destroy(gameObject);
+        score.score += 2;
+        sp.countDestroy++;
+    }
+
+
+    void Update()
+    {
+
+        _timer += Time.deltaTime;
+
+        transform.localScale *= (1 + Time.deltaTime / (sp.speed*0.3f));
+
+        if (_timer > sp.speed*0.3)
+        {
+            Destroy(gameObject);
+        }
+
+        if (sp.countDestroy >= 10 &&
+            sp.speed > 1)
+        {
+            sp.speed -= 0.5f;
+            sp.countDestroy = 0;
+
+        }
+
+    }
+}
